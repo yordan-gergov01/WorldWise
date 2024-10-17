ff7ff6526050b761a30047dceb68957a26732bdd
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-17 07:41:44 +0300

    Initial implement react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
