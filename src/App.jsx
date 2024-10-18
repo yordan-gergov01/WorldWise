105be8040017974986adffb907707ccfd8eec355
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-18 07:46:10 +0300

    Build nested routes for cities and countries

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
         <Route path="login" element={<Login />} />
       </Routes>
