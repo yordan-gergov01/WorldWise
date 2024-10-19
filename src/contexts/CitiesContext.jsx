f56c73579fe424d30149a34f56ac0cc977360e77
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-19 17:34:06 +0300

    create deleting a city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -58,6 +58,20 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -66,6 +80,7 @@ function CitiesProvider({ children }) {
         getCity,
         currentCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
